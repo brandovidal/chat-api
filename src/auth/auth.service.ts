6c581c96a8ad9740a9aa036eeb3a91e068701860
Brando Vidal <yisusdracon@gmail.com>
2024-10-28 14:47:25 -0500

    build: create auth service and use bycrypt package

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { User, UserDocument } from 'src/schemas/user.schema';
+import { RegisterUserDto } from './dto/register-user.dto';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async register(registerUserDto: RegisterUserDto): Promise<User> {
+    const { username, password } = registerUserDto;
+
+    // check the user exist
+    const existingUser = await this.userModel.findOne({ username }).exec();
+    if (existingUser) {
+      throw new Error('user ALready exists');
+    }
+
+    const salt = await bcrypt.gentSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new this.userModel({ username, password: hashedPassword });
+    return newUser.save();
+  }
+}
